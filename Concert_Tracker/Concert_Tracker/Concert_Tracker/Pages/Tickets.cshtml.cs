ab7a3d71ad4e1080bde9b56a84e1006ebdb222d1
Luc <l.somhorst@student.fontys.nl>
2022-02-14 14:49:25 +0100

    Basic pages added

---
+++ Concert_Tracker/Concert_Tracker/Concert_Tracker/Pages/Tickets.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Concert_Tracker.Pages
+{
+    public class TicketsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
